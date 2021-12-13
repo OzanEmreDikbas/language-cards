20202c51a57766778cf2e2ae0e0c9b7a35426c5c
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-13 20:02:50 +0300

    3. commit.

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -5,28 +5,22 @@ console.log(categories);
 
 const Cards = () => {
     return (
+      <div className="container">
+        {/* <h1>Languages</h1> */}
         <div className="card-container">
-      {categories.map((card, index) => {
-        return (
-          <div className="cards" key={index}>
-            <img src={card.img} alt={card.name} />
-            <div className="card-over">
-              <p>{card.options}</p>
-            </div>
-          </div>
+          {categories.map((card, index) => {
+          return (
+            <div className="cards" key={index}>
+              <img src={card.img} alt={card.name} />
+              <div className="card-over">
+                <p>{card.options}</p>
+              </div>
+            </div> 
         );
-      })}
-            {/* tek data çekme!
-            <div>
-                <div>
-                    <h2>Card Title</h2>
-                    <img src="https://cdn.pixabay.com/photo/2017/06/07/15/47/new-york-city-2380683_960_720.jpg" alt="" />
-                    <div>
-                        <p>Card desc</p>
-                    </div>
-                </div>
-            </div> */}
-        </div>
+      })}                         
+        </div>        
+      </div>
+
     )
 };
 
