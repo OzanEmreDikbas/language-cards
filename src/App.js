a28a55cb7cecd2a07241f1484c797c63a150e2d2
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-14 15:33:37 +0300

    1. commit.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 
-import './App.css';
-import Cards from './components/cards/Cards';
-import Header from './components/header/Header';
-import Navbar from './components/navbar/Navbar';
+// import './App.css';
+// import Cards from './components/cards/Cards';
+// import Header from './components/header/Header';
+// import Navbar from './components/navbar/Navbar';
 
+// function App() {
+//   return (
+//     <div className="App">
+//         <Navbar />
+//         <Header />
+//         <Cards />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import Cards from "./components/cards/Cards";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import { useState } from "react";
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleShow = () => setIsVisible(!isVisible);
   return (
-    <div className="App">
-        <Navbar />
-        <Header />
-        <Cards />
+    <div className='App'>
+      <Navbar />
+      <Cards isVisible={isVisible} toggleShow={toggleShow} />
     </div>
   );
 }
-
 export default App;
